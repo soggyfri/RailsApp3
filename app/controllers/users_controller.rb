0c7944187e8185c10887833212aa1fc5533894d5
Frison Alexander <soggyfri@gmail.com>
2011-01-14 21:25:23 -0500

    finished new user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,24 @@
 class UsersController < ApplicationController
 
   def new
+    @user = User.new
     @title = "Welcome new user!"
   end
   
   def show
     @user = User.find(params[:id])
     @title = @user.name
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save 
+      flash[:success] = "Welcome to Uconn's own Facebook"
+      redirect_to @user
+    else
+      @title = "Sign up: Error"
+      render 'new'
+    end
   end
 
 end
